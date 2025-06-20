4976666f017e7a2ab5448f32cabe49c850eb6ee8
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2025-06-20 10:46:25 +0900

    update: add argsparser

--- src/view.py
+++ src/view.py
@@ -1,20 +1,27 @@
+import argparse
 import genesis as gs
+
+parser = argparse.ArgumentParser(description="Terrain Viewer")
+parser.add_argument(
+    "--path",
+    type=str,
+    default="pyramid_terrain.urdf",
+    help="Path to the URDF file of the terrain",
+)
+args = parser.parse_args()
 
 gs.init(backend=gs.gs_backend.cuda)
 
-
 scene = gs.Scene()
-
 scene.add_entity(
     morph=gs.morphs.URDF(
-        file="terrain.urdf",
+        file=args.path,
         pos=[0, 0, 0],
         fixed=True,
         scale=0.2,
     ),
     vis_mode="collision",
 )
-
 scene.build()
 
 while True:
